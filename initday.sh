ee535b831ac0ddb6d900c2b5751385b9f6e707ba
Bo Chen <bo@liftoff.io>
2023-12-03 21:48:26 -0800

    set up to use sample by default

--- initday.sh
+++ initday.sh
@@ -3,3 +3,4 @@ cp -n base.py "${1}/${1}a.py"
 
 SES=`cat ${HOME}/src/adventsession.txt`
 curl https://adventofcode.com/2023/day/$1/input --cookie "session=${SES}" > "${1}/in.txt"
+touch "${1}/s.txt" 
